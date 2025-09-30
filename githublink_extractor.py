f3bb1e52a83902135c0b9441c5daa530adafbb43
Zhenzhong Zhang <zhang3zz@ad.unc.edu>
2025-09-30 13:27:30 -0400

    update star data

--- githublink_extractor.py
+++ githublink_extractor.py
@@ -197,6 +197,7 @@ def githublink_extractor(data_path, gemini_api_key, input_file="arxiv.csv", outp
     )
     
     output_file_path = os.path.join(data_path, output_file)
+    output_file_path_backup = os.path.join(os.path.dirname(output_file_path), os.path.splitext(os.path.basename(output_file_path))[0] + "_backup" + os.path.splitext(output_file_path)[1])
     input_file_path = os.path.join(data_path, input_file)
 
     # If the output file does not exist, copy the input file and add a new column for GitHub links
@@ -205,10 +206,18 @@ def githublink_extractor(data_path, gemini_api_key, input_file="arxiv.csv", outp
         df = pd.read_csv(output_file_path)
         df["Github_Link"] = ""
     else:
+        shutil.copyfile(output_file_path, output_file_path_backup)
         df = pd.read_csv(output_file_path)
+        df_new = pd.read_csv(input_file_path)
+        if "Github_Link" not in df_new.columns:
+            df_new["Github_Link"] = ""
+        # Merge on 'ID' with outer join
+        df = pd.merge(df_new, df, on=["Arxiv_ID","Title","Pdf_Link","Published_Date"], how="outer", suffixes=('_x', '_y'))
+        # If right value exists, use it; otherwise use left
+        df['Github_Link'] = df['Github_Link_y'].combine_first(df['Github_Link_x'])
         # Ensure the Github_Link column exists
-        if "Github_Link" not in df.columns:
-            df["Github_Link"] = ""
+        df = df.drop(columns=["Github_Link_x", "Github_Link_y"])
+
 
     # Iterate over rows and fill in missing GitHub links
     for idx in tqdm(df.index, desc="Processing articles", unit="article"):
