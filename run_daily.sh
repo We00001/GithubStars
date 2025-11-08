1225daeb96cd516f3c82599681326bc6dfd2832e
Zhenzhong Zhang <zhang3zz@ad.unc.edu>
2025-11-08 08:49:57 -0500

    clean

--- run_daily.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 切换到项目目录
-cd /Users/zhenzhongzhang/project1 || exit
-
-# 激活虚拟环境
-source /Users/zhenzhongzhang/project1/.venv/bin/activate
-
-# 执行 Python 脚本并记录日志
-/Users/zhenzhongzhang/project1/.venv/bin/python /Users/zhenzhongzhang/project1/dataset_update.py >> /Users/zhenzhongzhang/project1/daily.log 2>&1
